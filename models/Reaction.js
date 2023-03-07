213ae006c249d186c8d0bb86f67494174ad8fd4a
KobyS82 <kobysillito@gmail.com>
2023-03-06 21:37:21 -0600

    added reaction model

---
+++ models/Reaction.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require("mongoose");
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+
+        },
+        reactionBody: {
+            type: String,
+            required: "You need to provide a reaction!",
+            minlength: 1,
+            maxlength: 280,
+        },
+        username: {
+            type: String,
+            required: "You need to provide a username!",
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => dateFormat(createdAtVal),
+        },
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+    }
+);
+
+//delete this line? Don't think I need it
+// README says that it is schema only
+const Reaction = model("Reaction", reactionSchema);
+
+module.exports = Reaction;
